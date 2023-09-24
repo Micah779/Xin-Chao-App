85ac358929428409e961f5c9b784fa309b721eb4
Micah779 <contactmaldrich@gmail.com>
2023-09-23 22:39:32 -0400

    Initial Commit

---
+++ Xin Chào/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Xin Chào
+//
+//  Created by Micah Aldrich on 9/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xin Chào/Xin_Cha_oApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xin_Cha_oApp.swift
+//  Xin Chào
+//
+//  Created by Micah Aldrich on 9/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Xin_Cha_oApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
