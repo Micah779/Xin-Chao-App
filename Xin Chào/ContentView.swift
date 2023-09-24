f6579c91fd7d96e1fa1eb5214067424a0af1ab77
Micah779 <contactmaldrich@gmail.com>
2023-09-24 00:06:54 -0400

    First Version of the Vietnamese Vocabulary App. I plan to expand upon this as I learn more Swift Code.

--- Xin Chào/ContentView.swift
+++ Xin Chào/ContentView.swift
@@ -8,16 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var theCardNum: Int = 1
+    @State var theCard: String = "c1"
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.white)
+                .opacity(0.8)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image("xinchao_logo2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Spacer()
+                Image(theCard)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(20)
+                    .padding()
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: backCard) {
+                        Text("BACK")
+                            .font(.title2)
+                            .foregroundColor(Color.black)
+                            .fontWeight(.bold)
+                            .padding()
+                            .border(.black)
+                    }
+                    Spacer()
+                    Button(action: resetCard) {
+                        Text("RESET")
+                            .font(.title2)
+                            .foregroundColor(Color.black)
+                            .fontWeight(.bold)
+                            .padding()
+                            .border(.black)
+                    }
+                    Spacer()
+                    Button(action: flipCard) {
+                        Text("FLIP")
+                            .font(.title2)
+                            .foregroundColor(Color.black)
+                            .fontWeight(.bold)
+                            .padding()
+                            .border(.black)
+                    }
+                    Spacer()
+                    
+                }
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func flipCard() {
+        if (theCardNum < 100){
+            theCardNum += 1
+            theCard = "c\(theCardNum)"
+        }
+    }
+    func backCard() {
+        if (theCardNum > 1){
+            theCardNum -= 1
+            theCard = "c\(theCardNum)"
+        }
+    }
+    func resetCard() {
+        theCardNum = 1
+        theCard = "c\(theCardNum)"
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
